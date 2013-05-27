940a4166763caeecab23b63b7793900678b622ed
George Barnick <george.barnick@brickcraft.me>
2013-05-27 19:15:20 -0400

    Import from server

---
+++ serialized/conf-20130301151623.php
@@ -0,0 +1,60 @@
+<?php $settings = array (
+  'default' => 
+  array (
+    'wgAutopromote' => 
+    array (
+      '' => 
+      array (
+        0 => 2,
+        1 => -1,
+      ),
+    ),
+    'wgAddGroups' => 
+    array (
+      'sysop' => 
+      array (
+        0 => 'rollback',
+        1 => 'patroller',
+      ),
+      'bureaucrat' => 
+      array (
+        0 => 'bot',
+        1 => 'sysop',
+        2 => 'bureaucrat',
+        3 => 'rollback',
+        4 => 'patroller',
+        5 => 'forumadmin',
+      ),
+    ),
+    'wgRemoveGroups' => 
+    array (
+      'sysop' => 
+      array (
+        0 => 'rollback',
+        1 => 'patroller',
+      ),
+      'bureaucrat' => 
+      array (
+        0 => 'bot',
+        1 => 'sysop',
+        2 => 'bureaucrat',
+        3 => 'rollback',
+        4 => 'patroller',
+        5 => 'forumadmin',
+      ),
+    ),
+    'wgExtraNamespaces' => 
+    array (
+      105 => 'Board',
+    ),
+    '__includes' => 
+    array (
+    ),
+  ),
+  '__metadata' => 
+  array (
+    'user_wiki' => 'brickimedia_meta',
+    'user_name' => 'Adrian (Brickimedia)',
+    'reason' => 'adding board namespace',
+  ),
+);
