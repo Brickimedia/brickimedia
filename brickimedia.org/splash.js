0ea27d6926244a96e5fe7b818ba658043e733ff9
George Barnick <george.barnick@brickcraft.me>
2013-07-31 00:28:22 -0400

    updating splash
    someone else please add content to the news and connect pages if you can

---
+++ brickimedia.org/splash.js
@@ -0,0 +1,16 @@
+var mainText = "<span class='big'>Welcome &nbsp;to&nbsp; <b>Brickimedia</b>&nbsp;<small><sup><i>ßeta</i></sup></small></span><br/><br/><span class='small'>An open project to create the largest LEGO fan network on the web</span>";
+var aboutDowntime = "<span class='small'>About our downtime</span><br/><br/>Brickimedia is currently offline due to a recent attack on our server host. As a result, we lost all content on our server, and now have to start over. Although this is inconvenient, it's the only option we have in order to bring our service back into the public.<br /><br />Some changes that you will notice taking place include a refreshed user experience, with easier navigation, editing capability, and customization of the site for your needs. We will also be lessening the amount of projects we host for the time being. From now on, you may access the LEGO Message Boards Wiki <a href='http://legomessageboards.wikia.com'>on Wikia</a>.<br/><br/><img src='img/back.svg' width='30px' height='30px' onClick='defaultText()' style='float:right;cursor:pointer;' />";
+var newsText = "<span class='small'>What to expect</span><br/><br/>Placeholder<br/><br/>Give some info on whatever<br/><br/><img src='img/back.svg' width='30px' height='30px' onClick='defaultText()' style='float:right;cursor:pointer;' />";
+var stayTuned = "<span class='small'>Stay tuned</span><br/><br/>Placeholder<br/><br/>Social links will go here and stuff<br/><br/><img src='img/back.svg' width='30px' height='30px' onClick='defaultText()' style='float:right;cursor:pointer;' />";
+function defaultText() {
+	document.getElementById('middle').innerHTML = mainText;
+};
+function downtime() {
+	document.getElementById('middle').innerHTML = aboutDowntime;
+};
+function news() {
+	document.getElementById('middle').innerHTML = newsText;
+};
+function connect() {
+	document.getElementById('middle').innerHTML = stayTuned;
+};
