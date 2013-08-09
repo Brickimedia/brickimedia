6aa26e7cb651d26cd0916457cb2f149602d84e0b
maestro35 <maestro35@outlook.com>
2013-08-08 20:11:06 -0400

    reconfiguring skin php

--- wiki/skins/Custard.php
+++ wiki/skins/Custard.php
@@ -23,8 +23,8 @@ $wgExtensionCredits[$type][] = array(
 );
 
 $wgValidSkinNames['custard'] = 'Custard';
-$wgAutoloadClasses['SkinCustard'] = dirname(__FILE__).'/Custard.skin.php';
-$wgExtensionMessagesFiles['Custard'] = dirname(__FILE__).'/Custard.i18n.php';
+//$wgAutoloadClasses['SkinCustard'] = dirname(__FILE__).'/Custard.skin.php';
+//$wgExtensionMessagesFiles['Custard'] = dirname(__FILE__).'/Custard.i18n.php';
 
 $wgResourceModules['skins.custard'] = array(
         'styles' => array(
@@ -33,5 +33,110 @@ $wgResourceModules['skins.custard'] = array(
         'remoteBasePath' => &$GLOBALS['wgStylePath'],
         'localBasePath' => &$GLOBALS['wgStyleDirectory'],
 );
+
+?>
+
+<?php
+
+/**
+* Skin file for skin My Skin.
+*
+* @file
+* @ingroup Skins
+*/
+
+/**
+ * SkinTemplate class for My Skin skin
+ * @ingroup Skins
+ */
+class SkinCustard extends SkinTemplate {
+ 
+		var $skinname = 'custard', $stylename = 'custard',
+				$template = 'CustardTemplate', $useHeadElement = true;
+ 
+		/**
+		 * @param $out OutputPage object
+		 */
+		function setupSkinUserCss( OutputPage $out ){
+				parent::setupSkinUserCss( $out );
+				$out->addModuleStyles( "skins.custard" );
+		}
+ 
+}
+
+/**
+ * BaseTemplate class for My Skin skin
+ * @ingroup Skins
+ */
+class CustardTemplate extends BaseTemplate {
+ 
+		/**
+		 * Outputs the entire contents of the page
+		 */
+		public function execute() {
+				// Suppress warnings to prevent notices about missing indexes in $this->data
+				wfSuppressWarnings();
+
+				$this->html( 'headelement' ); ?>
+				
+				<div id="toolbar">
+					<div class="toggle"></div>
+					<div id="mw-js-message" class="message" style="display:none;"></div>
+					<?php if ( $this->data['sitenotice'] ) { ?>
+						<div id="site-notice">
+							<?php $this->html( 'sitenotice' ); ?>
+						</div>
+					<?php } ?>
+					<?php if ( $this->data['newtalk'] ) { ?>
+						<div id="new-talk" class="message">
+							<?php $this->html( 'newtalk' ); ?>
+						</div>
+					<?php } ?>
+				</div>
+				
+				<div id="interwiki">
+					<div id="left" class="sub">
+						<a href="#meta">Meta</a>
+						<a href="#pedia">Pedia</a>
+					</div>
+					<div id="mid" class="sub">
+						<a href="#main">Brickimedia</a>
+					</div>
+					<div id="right" class="sub">
+						<a href="#lmbw">LMBW</a>
+						<a href="#stories">Stories</a>
+					</div>
+				</div>
+				
+				<div id="page">
+					<div class="tabs"></div>
+					<h1 id="header"><?php $this->html( 'title' ); ?></h1>
+					<?php if ( $this->data['subtitle'] ) { ?>
+						<div class="sub-header">
+							<?php $this->html( 'subtitle' ); ?>
+						</div>
+					<?php } ?>
+					<?php if ( $this->data['undelete'] ) { ?>
+						<div id="sub-header">
+							<?php $this->html( 'undelete' ); ?>
+						</div>
+					<?php } ?>
+					<div id="content">
+						<?php $this->html( 'bodytext' ) ?>
+					</div>
+					<?php $this->html( 'catlinks' ); ?>
+				</div>
+				
+				<?php $this->printTrail(); ?>
+				
+				</body>
+				</html>
+				
+				<?php
+				
+				wfRestoreWarnings();
+		}
+
+}
 
 ?>
