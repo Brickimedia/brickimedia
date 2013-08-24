08da1c546cb4634646242630ff5f099344061e58
maestro35 <maestro35@outlook.com>
2013-08-24 09:40:11 -0400

    still trying to fix resources bug

--- wiki/skins/Quartz.php
+++ wiki/skins/Quartz.php
@@ -130,9 +130,6 @@ class QuartzTemplate extends BaseTemplate {
 		// Output HTML Page
 		$this->html( 'headelement' );
 ?>
-<?php
-    require_once('$IP/skins/quartz/resources.php');
-?>
 		<!-- header -->
 		<div id="mw-head" class="noprint">
 			<div id="p-interwiki">
