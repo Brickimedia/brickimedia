e58005aa96fa6e3872b69b89cc9fa1ae4bdcaa18
maestro35 <maestro35@outlook.com>
2013-08-24 09:41:40 -0400

    closing php tag

--- wiki/skins/quartz/resources.php
+++ wiki/skins/quartz/resources.php
@@ -12,4 +12,4 @@ $wgResourceModules['skins.quartz'] = array(
     ),
     'remoteBasePath' => $GLOBALS['wgStylePath'],
     'localBasePath' => $GLOBALS['wgStyleDirectory']
-);require_once( "$IP/extensions/Verbatim/verbatim.php" );
+);require_once( "$IP/extensions/Verbatim/verbatim.php" );?>
