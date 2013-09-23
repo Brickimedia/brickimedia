bb78ebded8c63270202d7c9c2f8682869769f474
George Barnick <george.barnick@brickcraft.me>
2013-09-23 16:04:10 -0400

    Skin updates
    CSS fix for Refreshed, and footer links to Terms of Use

--- wiki/LocalSettings_ext.php
+++ wiki/LocalSettings_ext.php
@@ -86,6 +86,35 @@ $wgFooterIcons['brickimedia']['brickimedia'] = array(
 "width" => "88"
 );
 
+# Terms of Use link in footer
+$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'lfTOULink';
+function lfTOULink( $sk, &$tpl ) {
+        $tpl->set( 'termsofuse', $sk->footerLink( 'termsofuse', 'termsofusepage' ) );
+        $tpl->data['footerlinks']['places'][] = 'termsofuse';
+        return true;
+}
+$messages['en'] = array(
+	'termsofuse' => 'Terms of Use',
+	'termsofusepage' => 'm:Terms of Use'
+);
+$messages['fr'] = array(
+	'termsofuse' => 'Conditions d\'utilisation',
+	'termsofusepage' => 'm:Terms of Use'
+);
+$messages['de'] = array(
+	'termsofuse' => 'Nutzungsbedingungen',
+	'termsofusepage' => 'm:Terms of Use'
+);
+$messages['pl'] = array(
+	'termsofuse' => 'Zasady Użytkowania',
+	'termsofusepage' => 'm:Terms of Use'
+);
+$messages['jp'] = array(
+	'termsofuse' => '利用規約',
+	'termsofusepage' => 'm:Terms of Use'
+);
+
+
 #Autoconfirmed
 $wgAutoConfirmAge = 86400*3;
 $wgAutoConfirmCount = 5;
