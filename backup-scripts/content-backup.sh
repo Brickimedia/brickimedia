bc956217f2adfc9bbaca8208053c5a7aec2ad607
Adam Carter <adamr_carter@btinternet.com>
2013-10-04 20:20:00 +0100

    add backup script

---
+++ backup-scripts/content-backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /media/backups/pass.sh
+
+databases=(bmshared brickimedia_meta brickimedia_en brickimedia_customs brickimedia_lmbw brickimedia_stories)
+locations=("/var/www/wiki/skins" "/var/www/wiki/extensions")
+
+echo "backing up dbs"
+
+for db in ${databases[*]}
+do
+	echo "backing up " $db
+	mysqldump -h localhost --user=brickimedia_db --password=Kohphuu1 $db | gzip > /media/backups/$db.sql.gz --force
+done
+
+echo "backing up files"
+
+for path in ${locations[*]}
+do
+	file=`echo $path | cut -d/ -f5`
+	echo "backing up " $file
+	newpath=/media/backups/$file.tar.gz
+	tar -zcf $newpath $path --overwrite
+done
