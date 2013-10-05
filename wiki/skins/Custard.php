dd4a203538744e8d8f176f6d03163078285ce8ab
maestro35 <maestro35@outlook.com>
2013-10-05 18:50:30 -0400

    fixing svg fail

--- wiki/skins/Custard.php
+++ wiki/skins/Custard.php
@@ -101,7 +101,7 @@ class CustardTemplate extends BaseTemplate
                 <div class="level module medium">Level</div>
                 <div class="chat module medium">Chat</div>
                 <div class="user module medium">User</div>
-                <div class="preferences module narrow"><img src="skins/custard/Images/gear.svg" alt="Preferences"/></div>
+                <div class="preferences module narrow"><embed src="$IP/skins/custard/Images/gear.svg" type="image/svg+xml" /></div>
             </div>
         </div>
 
