0d442d15d4f6664f401fa1d4590d2da2db1e8a70
Adam Carter <adamr_carter@btinternet.com>
2013-10-06 08:43:34 +0100

    these no longer need to be backed up; they are on github

--- backup-scripts/content-backup.sh
+++ backup-scripts/content-backup.sh
@@ -3,7 +3,6 @@
 source /media/backups/pass.sh
 
 databases=(shared meta en customs lmbw stories cuusoo admin dev globalblocking)
-locations=("/var/www/wiki/skins" "/var/www/wiki/extensions" "/var/www/brickimedia.org")
 
 echo "backing up dbs"
 
@@ -11,14 +10,4 @@ for db in ${databases[*]}
 do
 	echo "backing up " $db
 	mysqldump -h localhost --user=$dbuser --password=$dbpass $db | gzip > /media/backups/$db.sql.gz --force
-done
-
-echo "backing up files"
-
-for path in ${locations[*]}
-do
-	file=`echo $path | rev | cut -d/ -f1 | rev`
-	echo "backing up " $file
-	newpath=/media/backups/$file.tar.gz
-	tar -zcf $newpath $path --overwrite
 done
