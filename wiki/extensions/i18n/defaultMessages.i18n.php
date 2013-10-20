0d19aece425451ccdbeebac57b0b83344bc5c060
George Barnick <george.barnick@brickcraft.me>
2013-10-20 14:11:23 -0400

    Create defaultMessages.i18n.php

---
+++ wiki/extensions/i18n/defaultMessages.i18n.php
@@ -0,0 +1,14 @@
+<?php
+# This file holds messages that are system default but contain non-default values
+$messages['en'] = array(
+        'privacypage' => 'm:Privacy Policy'
+);
+$messages['fr'] = array(
+        'privacypage' => 'm:Privacy Policy'
+);
+$messages['es'] = array(
+        'privacypage' => 'm:Privacy Policy'
+);
+$messages['de'] = array(
+        'privacypage' => 'm:Privacy Policy'
+);
