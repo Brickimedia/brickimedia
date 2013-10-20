b4921eb8a563a5dac9b4671e7321c0af6d7a0e82
George Barnick <george.barnick@brickcraft.me>
2013-10-20 13:29:33 -0400

    Create TermsOfUse.i18n.php

---
+++ wiki/extensions/i18n/TermsOfUse.i18n.php
@@ -0,0 +1,26 @@
+<?php
+$messages = array();
+$messages['en'] = array(
+        'termsofuse' => 'Terms of Use',
+        'termsofusepage' => 'm:Terms of Use'
+);
+$messages['fr'] = array(
+        'termsofuse' => 'Conditions d\'utilisation',
+        'termsofusepage' => 'm:Terms of Use'
+);
+$messages['de'] = array(
+        'termsofuse' => 'Nutzungsbedingungen',
+        'termsofusepage' => 'm:Terms of Use'
+);
+$messages['pl'] = array(
+        'termsofuse' => 'Zasady użytkowania',
+        'termsofusepage' => 'm:Terms of Use'
+);
+$messages['jp'] = array(
+        'termsofuse' => '利用規約',
+        'termsofusepage' => 'm:Terms of Use'
+);
+$messages['es'] = array(
+        'termsofuse' => 'Términos de uso',
+        'termsofusepage' => 'm:Terms of Use'
+);
