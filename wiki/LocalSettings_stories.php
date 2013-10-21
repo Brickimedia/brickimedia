82f1c319ce40684bf44541f92f40af86040101ac
Oliver <SirComputer1@users.noreply.github.com>
2013-10-21 19:11:22 +0100

    I assume if Lia is disabled, LiaMobile should (although it is a very nice skin).

--- wiki/LocalSettings_stories.php
+++ wiki/LocalSettings_stories.php
@@ -11,7 +11,7 @@ $wgLanguageCode = "en";
 #Skin stuff
 require_once( "$IP/skins/quartz/resources.php" );
 $wgDefaultSkin = 'quartz';
-$wgSkipSkins = array("lia", "standard");
+$wgSkipSkins = array("lia", "liamobile", "standard");
 
 #SocialProfile
 require_once("$IP/extensions/SocialProfile/SocialProfile.php");
