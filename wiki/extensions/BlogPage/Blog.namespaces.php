20cb493539c0d762e8426277eb2a28dd5c912d97
George Barnick <george.barnick@brickcraft.me>
2013-11-11 21:17:15 -0500

    Adjustment to close #74

--- wiki/extensions/BlogPage/Blog.namespaces.php
+++ wiki/extensions/BlogPage/Blog.namespaces.php
@@ -12,54 +12,42 @@ if( !defined( 'NS_BLOG' ) ) {
 	define( 'NS_BLOG', 500 );
 }
 
-if( !defined( 'NS_BLOG_TALK' ) ) {
-	define( 'NS_BLOG_TALK', 501 );
-}
-
 /** English */
 $namespaceNames['en'] = array(
-	NS_BLOG => 'Blog',
-	NS_BLOG_TALK => 'Blog_talk',
+	NS_BLOG => 'Blog'
 );
 
 /** German (Deutsch) */
 $namespaceNames['de'] = array(
-	NS_BLOG => 'Blog',
-	NS_BLOG_TALK => 'Blog_Diskussion'
+	NS_BLOG => 'Blog'
 );
 
 /** Spanish (Español) */
 $namespaceNames['es'] = array(
-	NS_BLOG => 'Blog',
-	NS_BLOG_TALK => 'Blog_Discusión'
+	NS_BLOG => 'Blog'
 );
 
 /** Finnish (Suomi) */
 $namespaceNames['fi'] = array(
-	NS_BLOG => 'Blogi',
-	NS_BLOG_TALK => 'Keskustelu_blogista',
+	NS_BLOG => 'Blogi'
 );
 
 /** Dutch (Nederlands) */
 $namespaceNames['nl'] = array(
-	NS_BLOG => 'Blog',
-	NS_BLOG_TALK => 'Overleg_blog',
+	NS_BLOG => 'Blog'
 );
 
 /** Norwegian Nynorsk (‪Norsk (nynorsk)‬) */
 $namespaceNames['nn'] = array(
-	NS_BLOG => 'Blogg',
-	NS_BLOG_TALK => 'Bloggdiskusjon'
+	NS_BLOG => 'Blogg'
 );
 
 /** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬) */
 $namespaceNames['no'] = array(
-	NS_BLOG => 'Blogg',
-	NS_BLOG_TALK => 'Bloggdiskusjon'
+	NS_BLOG => 'Blogg'
 );
 
 /** Russian (Русский) */
 $namespaceNames['ru'] = array(
-	NS_BLOG => 'Блог',
-	NS_BLOG_TALK => 'Обсуждение_блога'
+	NS_BLOG => 'Блог'
 );
