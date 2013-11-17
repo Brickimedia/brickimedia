03dd1b8c8502f04dc06f72b42ab2014a6d33cd70
Lewis Cawte <lewis@lewiscawte.me>
2013-11-17 21:11:19 +0000

    Quartz now has it's own repo at https://github.com/Brickimedia/Quartz.

--- wiki/skins/quartz/resources.php
+++
@@ -1,15 +0,0 @@
-<?php
-$wgResourceModules['skins.quartz'] = array(
-    'styles' => array(
-        "$IP/skins/common/commonElements.css" => array( 'media' => 'screen' ),
-        "$IP/skins/common/commonContent.css" => array( 'media' => 'screen' ),
-        "$IP/skins/common/commonInterface.css" => array( 'media' => 'screen' ),
-        "$IP/skins/quartz/screen.css" => array( 'media' => 'screen' ),		"$IP/skins/quartz/player/citrine.css" => array( 'media' => 'screen' )
-    ),
-    'scripts' => array(		"$IP/resources/jquery.effects/jquery.effects.core.js",		"$IP/resources/jquery.effects/jquery.effects.drop.js",
-    	"$IP/skins/quartz/quartz.js",
-    	"$IP/skins/common/foes.js",		"$IP/skins/quartz/player/jquery.jplayer.min.js",		"$IP/skins/quartz/player/jplayer.playlist.min.js",		"$IP/skins/quartz/player/player.js"
-    ),
-    'remoteBasePath' => $GLOBALS['wgStylePath'],
-    'localBasePath' => $GLOBALS['wgStyleDirectory']
-);require_once( "$IP/extensions/Verbatim/verbatim.php" );?>
