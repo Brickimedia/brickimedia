807819e074847e1c277056d9fc6ec24735132ef9
maestro35 <maestro35@outlook.com>
2013-12-27 17:45:15 -0500

    enabling Custard again

--- wiki/LocalSettings.php
+++ wiki/LocalSettings.php
@@ -206,7 +206,7 @@ require_once( $ls_path );
 require_once( "$IP/skins/Refreshed/Refreshed.php" );
 $wgDefaultSkin = 'refreshed';
 
-require_once( "$IP/skins/Custard.php" );
+require_once( "$IP/skins/Custard/Custard.php" );
 
 $wgGroupPermissions['*']['edit'] = true;
 $wgGroupPermissions['user']['edit'] = true;
